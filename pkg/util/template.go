a43d0af0152841269c18912a46befbe2dfb23992
bugrakocabay <kocabaybugra@gmail.com>
2023-12-17 08:53:07 +0300

    feat: implement graphql template func

--- pkg/util/template.go
+++ pkg/util/template.go
@@ -36,3 +36,27 @@ func ProcessTemplate(template map[string]interface{}, messageData map[string]int
 
 	return bodyBytes, nil
 }
+
+// ProcessGraphQLTemplate processes the graphql template and returns the processed body
+func ProcessGraphQLTemplate(graphqlTemplate string, messageData map[string]interface{}) (string, error) {
+	processedQuery := graphqlTemplate
+
+	for key, value := range messageData {
+		placeholder := fmt.Sprintf("{{%s}}", key)
+		var valueStr string
+
+		// Convert different types to their string representations
+		switch v := value.(type) {
+		case string:
+			valueStr = fmt.Sprintf("\"%s\"", v)
+		case int, float64, bool:
+			valueStr = fmt.Sprintf("%v", v)
+		default:
+			return "", fmt.Errorf("unsupported type for key %s", key)
+		}
+
+		processedQuery = strings.Replace(processedQuery, placeholder, valueStr, -1)
+	}
+
+	return processedQuery, nil
+}
